2a3503a1e7b1aab6f68e3ddca65aaae9a2fe8608
Kilkenni <hpittman89@outlook.com>
2021-11-10 04:55:10 +0200

    task 9, required part complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonElem = document.querySelector(`button[class="change-color"]`);
+const colorTextElem = document.querySelector(`span[class="color"]`);
+
+function newBkgColor() {
+  const newColor = getRandomHexColor();
+
+  colorTextElem.textContent = newColor;
+  document.body.style.backgroundColor = newColor; //magic!
+}
+
+console.log(buttonElem);
+
+buttonElem.addEventListener("click", newBkgColor)
